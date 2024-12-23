cbbdc949b36a16f42eccd5fa1eda64905d5bd954
Tyler Liu <tyler4long@gmail.com>
2024-12-23 15:09:56 -0800

    Update code

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Shared worker has a variable `connectionCount`. 
 If it keep increasing when you refresh the page, it means the shared worker survived page refresh.
 Because if shared worker didn't survive page refresh, `connectionCount` would be reset to 0.
+
+
+## Conclusion
+
+There is no easy way.
--- shared-worker.js
+++ shared-worker.js
@@ -1,14 +1,37 @@
-let connectionCount = 0;
+let timestamp = new Date().toISOString();
+let popupPort = null;
+const ports = new Set();
 
 self.onconnect = (event) => {
     const port = event.ports[0];
-    connectionCount++;
-
-    port.postMessage(`Connected. Current connection count: ${connectionCount}`);
 
     port.onmessage = (messageEvent) => {
-        if (messageEvent.data === 'get-count') {
-            port.postMessage(`Current connection count: ${connectionCount}`);
+      if(messageEvent.data === 'index.html') {
+        ports.add(port);
+        if(popupPort && ports.size > 1) {
+          popupPort.postMessage('close');
+          popupPort = null;
         }
+        port.postMessage(`timestamp: ${timestamp}`);
+        if(ports.size === 1 && !popupPort) {
+          port.postMessage('open-popup');
+        }
+      } else if(messageEvent.data === 'popup.html') {
+        if(popupPort) {
+          popupPort.postMessage('close');
+        }
+        popupPort = port;
+      }
+    };
+
+    port.onclose = () => {
+      if(popupPort === port) {
+        popupPort = null;
+      } else {
+        ports.delete(port);
+      }
+      if(ports.size === 1 && !popupPort) {
+        ports.values().next().value.postMessage('open-popup');
+      }
     };
 };
